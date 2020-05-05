614875915a2378346daa7a5c7ffff9068b8a810e
mehdi <mehdi_adeli@aut.ac.ir>
2020-05-05 12:54:04 +0000

    Add chapter 2 source code

---
+++ Chapter 2 Variables/FirstProject/src/package1/Class1.java
@@ -0,0 +1,49 @@
+package package1;
+/*
+I like Itpro
+من آیتی پرو را دوست دارم
+ */
+//sdlkf ;djfs;lk
+/**
+ * sldfkjs
+ *
+ * type name=value;
+ * int
+ * byte
+ * long
+ * float
+ * double
+ * boolean
+ */
+public class Class1 {
+    public static void main(String[] args) {
+        System.out.println("salam Itpor");
+        System.out.println("dobare salam");
+
+        int num1=10034;
+        int num2=-54;
+        byte b1=102;
+        long ln1=12345678900L;
+        float float1=23.5f;
+        double double1=556.768;
+        boolean b=true;
+        String name="Mohammad";
+        String lastName="Nasiri";
+        String website = "Itpro";
+        String space = " ";
+        char ch='u';
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(b1);
+        System.out.println(ln1);
+        System.out.println(float1);
+        System.out.println(double1);
+        System.out.println(b);
+        System.out.println(name);
+        System.out.println(lastName);
+        System.out.println(website);
+        System.out.println("Mehdi"+"\n"+lastName+" "+website+space+num1+space+double1);
+
+    }
+}
